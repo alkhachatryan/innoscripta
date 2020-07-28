78984f255f68010eee54b8ddfb0f0f2794d87a50
alexey <alexey.khachatryan@gmail.com>
2020-07-28 17:34:07 +0400

    Created order, order_item migrations and models

---
+++ database/migrations/2020_07_28_130345_create_order_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedSmallInteger('quantity')->default(1);
+            $table->unsignedSmallInteger('price');  // Store the product in this table, as it can be changed after an order
+
+            // The product element should not be duplicated, instead of it increment the quantity
+            $table->unique(['order_id', 'product_id']);
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
